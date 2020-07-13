b76f6208552f806213e8b2c50b3c3801f8d3ec2b
Adedapomola Adekoya <adedapomola.adekoya@konga.com>
2020-07-12 21:10:55 -0600

    Updated Main file

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/a-cubed/module-s3-webapp/private"
+
+module "module-aws-s3-webapp" {
+  source  = "app.terraform.io/a-cubed/module-aws-s3-webapp/private"
+  version = "1.0.0"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
 }
