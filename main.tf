b5dad2f6ea5f8d4c76381107e812266d753840bb
Adedapomola Adekoya <adedapomola.adekoya@konga.com>
2020-07-12 21:01:56 -0600

    added module

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/a-cubed/module-s3-webapp/private"
   name        = var.name
   region = var.region
   prefix = var.prefix
